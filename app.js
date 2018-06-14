a72479de844f8a7bb595aaf0efe39a7e2fa89a2e
khatanark <rohitkhatana16@gmail.com>
2018-06-15 01:50:03 +0530

    adding middleware

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ const express =require('express');
 const app= express();
 // we need to call the express function.
 
- 
+//How middleware works.It is any piece code which has the access to the req and res objects
+//app.use() is used to use a middleware. 
+app.use(function(req,res,next){
+console.log(Date.now());
+next();
+});
 
 // Adding routes.
 // index routes 
@@ -16,9 +21,7 @@ app.get('/about', (req,res)=>{
 res.send('ABOUT');
 });
 
-
 const port =5000;
-
 app.listen(port, ()=>{
 	console.log(`server strated on port ${port}`); 
 });
